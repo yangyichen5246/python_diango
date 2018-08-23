1cd3ed090438ad5e4a7c7a20fcb2ce7dfae2cbb9
guying5246 <1073444857@qq.com>
2018-08-23 13:47:01 +0800

    update

--- login/models.py
+++ login/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class UserInfo(models.Model):
     user = models.CharField(max_length=100)
     pwd = models.CharField(max_length=32)
+    # mobie =models.CharField(max_length=11)
+
 
 # Create your models here.
